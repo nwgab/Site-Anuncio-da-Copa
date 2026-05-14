1d028226715491b35efc228e38e6a29e2ed493a9
Gabriel Damacena <damacenagab@gmail.com>
2026-05-14 15:31:28 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Servir arquivos estáticos (CSS, Imagens)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
